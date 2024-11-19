f6d3e605632305bbbc31b47f68815efb06d5961b
dawitYenew.12 <dawityenew.12@gmail.com>
2024-11-20 00:43:51 +0300

    test commit

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,9 +1,26 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import js from '@eslint/js';
+import prettier from 'eslint-plugin-prettier';
+import security from 'eslint-plugin-security';
 
-
-/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    plugins: {
+      prettier,
+      security,
+    },
+    rules: {
+      'no-console': 'error',
+      'no-unused-vars': 'off',
+      'no-undef': 'off',
+      'prettier/prettier': [
+        'warn',
+        {
+          endOfLine: 'auto', // Correct Prettier config for end-of-line
+        },
+      ],
+      'security/detect-object-injection': 'off',
+    },
+  },
 ];
