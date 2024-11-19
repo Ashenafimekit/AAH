b14e77becbfbf5b6f12abfde9cd368a48db3bc25
dawitYenew.12 <dawityenew.12@gmail.com>
2024-11-20 00:46:53 +0300

    test 2

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,14 @@ import express from 'express';
 import http from 'http';
 
 const startServer = async () => {
-    const app = express();
-}
+  const app = express();
+};
 
 (async () => {
-    try {
-        await startServer();
-    } catch (error) {
-        console.error('Error starting server');
-        // process.exit(1);
-        throw new Error(error);
-    }
+  try {
+    await startServer();
+  } catch (error) {
+    // process.exit(1);
+    throw new Error(error);
+  }
 })();
--- backend/models/booking.schema.js
+++ backend/models/booking.schema.js
@@ -1,44 +1,44 @@
 import { Schema, model } from 'mongoose';
 
 const bookingSchema = new Schema({
-    Name: {
-        type: String,
-        required: true,
-    },
-    room: {
-        type: Schema.Types.ObjectId,
-        ref: 'Room',
-        required: true,
-    },
-    checkIn: {
-        type: Date,
-        required: true,
-    },
-    checkOut: {
-        type: Date,
-        required: true,
-    },
-    NumberOfRoom: {
-        type: Number,
-        required: true,
-    },
-    Adults: {
-        type: Number,
-        required: true,
-    },
-    children: {
-        type: Number,
-        required: true,
-    },
-    notes: {
-        type: String,
-        required: true,
-    },
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        required: false,
-    },
+  Name: {
+    type: String,
+    required: true,
+  },
+  room: {
+    type: Schema.Types.ObjectId,
+    ref: 'Room',
+    required: true,
+  },
+  checkIn: {
+    type: Date,
+    required: true,
+  },
+  checkOut: {
+    type: Date,
+    required: true,
+  },
+  NumberOfRoom: {
+    type: Number,
+    required: true,
+  },
+  Adults: {
+    type: Number,
+    required: true,
+  },
+  children: {
+    type: Number,
+    required: true,
+  },
+  notes: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: false,
+  },
 });
 
 export default model('Booking', bookingSchema);
