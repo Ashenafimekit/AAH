d832c22d247e6a09e77bf2d41eea34ff9fe0871c
dawitYenew.12 <dawityenew.12@gmail.com>
2024-11-24 00:09:50 +0300

    Your commit message here

--- src/Pages/Room_Page.jsx
+++ src/Pages/Room_Page.jsx
@@ -3,6 +3,9 @@ import Header2 from '../Components/Header2'
 import Room from '../Components/Room'
 import Testimonial from '../Components/Testimonial'
 import Footer from '../Components/Footer'
+import React from "react";
+import Header2 from "../Components/Header2";
+import Body from "../Components/room/Body";
 
 const RoomClicked_Page = () => {
   return (
@@ -14,5 +17,11 @@ const RoomClicked_Page = () => {
     </div>
   )
 }
+    <div className="flex flex-col gap-5">
+      <Header2 title={"single room"} />
+      <Body />
+    </div>
+  );
+};
 
-export default RoomClicked_Page;
+export default Room_Page;
