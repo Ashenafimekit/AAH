3cb17c57c6001718c9df77cd52c4fa53cb0f3635
dawitYenew.12 <dawityenew.12@gmail.com>
2024-11-24 00:10:33 +0300

    staging room page

--- src/Pages/Room_Page.jsx
+++ src/Pages/Room_Page.jsx
@@ -7,7 +7,7 @@ import React from "react";
 import Header2 from "../Components/Header2";
 import Body from "../Components/room/Body";
 
-const RoomClicked_Page = () => {
+const Room_Page = () => {
   return (
     <div className='flex flex-col gap-5'>
       <Header2 title={'single room'}/>
@@ -16,12 +16,6 @@ const RoomClicked_Page = () => {
       <Footer/>   
     </div>
   )
-}
-    <div className="flex flex-col gap-5">
-      <Header2 title={"single room"} />
-      <Body />
-    </div>
-  );
 };
 
 export default Room_Page;
