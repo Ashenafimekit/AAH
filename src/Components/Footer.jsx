eac36c1c8581307e03508ed7f4b884117be313d8
Ashenafi Mekit <ashumekit502@gmail.com>
2024-11-23 16:29:43 +0300

    responsive design of home page

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,19 +3,19 @@ import { Facebook, Instagram, YouTube } from "@mui/icons-material";
 
 const Footer = () => {
   return (
-    <div className="flex flex-row items-center justify-around  gap-5 bg-[#14274A] text-white h-48 ">
-      <div className="flex flex-col gap-4">
+    <div className="flex flex-col md:flex-row items-center justify-center md:justify-around  gap-5 bg-[#14274A] text-white h-full ">
+      <div className="flex flex-col items-center justify-center gap-4">
         <div className="text-golden">
           <h1 className="font-bold text-xl ">Arab Ali Hotel</h1>
           <h1 className="font-semibold text-lg">فندق عرب علي</h1>
         </div>
-        <div>
+        <div className="flex flex-col items-center justify-center">
             <h1>497 Evergreen Rd. Roseville, CA 95673</h1>
             <h1>+251987654321</h1>
             <h1>arabalihotel@gmail.com</h1>
         </div>
       </div>
-      <div className="flex flex-col gap-3 text-white">
+      <div className="flex flex-row  md:flex-col gap-3 text-white">
         <h1 className="text-lg font-semibold">Our Social Medias</h1>
         <a href="" className="hover:text-[#E0B973]"><Facebook/> Facebook</a>
         <a href="" className="hover:text-[#E0B973]"><Instagram/> Instagram</a>
--- src/Components/HomePageContact.jsx
+++ src/Components/HomePageContact.jsx
@@ -27,19 +27,23 @@ const HomePageContact = () => {
   return (
     <div className="w-full flex items-center justify-center">
       <div className="flex flex-col md:flex-row items-start justify-center w-3/4 border border-gray-400 shadow-lg rounded-lg">
-        <div className="w-1/2">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6285.232305129996!2d42.10613233203546!3d9.31183314620799!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1631bd0d7297f53d%3A0x78ed31ae63ad8fbe!2sArab%20Ali%20Hotel!5e1!3m2!1sen!2set!4v1732215046751!5m2!1sen!2set"
-            width="503"
-            height="450"
-            loading="lazy"
-          ></iframe>
+        <div className="w-full md:w-1/2">
+          <div className="w-full h-[500px] rounded-lg overflow-hidden shadow-lg">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6285.232305129996!2d42.10613233203546!3d9.31183314620799!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1631bd0d7297f53d%3A0x78ed31ae63ad8fbe!2sArab%20Ali%20Hotel!5e1!3m2!1sen!2set!4v1732215046751!5m2!1sen!2set"
+              width="100%"
+              height="100%"
+              loading="lazy"
+              className="border border-black"
+              allowFullScreen
+            ></iframe>
+          </div>
         </div>
-        <div className="flex flex-col items-center justify-center gap-4 md:10 w-full md:w-1/2 mt-5">
+        <div className="flex flex-col items-center justify-center gap-4 md:gap-10 w-full md:w-1/2 mt-5">
           <h1 className="text-center font-bold text-2xl">Contact Us</h1>
           <form
             onSubmit={handleSubmit}
-            className="flex flex-col items-start justify-start gap-1 w-3/4 "
+            className="flex flex-col items-start justify-center gap-1 w-3/4 "
           >
             <label>Full Name</label>
             <input
