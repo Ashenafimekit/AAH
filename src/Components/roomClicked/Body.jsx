b55ae27a8aae11f46f79426bb962d0c69ed6d6a3
dawitYenew.12 <dawityenew.12@gmail.com>
2024-11-24 15:37:35 +0300

    booking form implemented

--- src/Components/roomClicked/Body.jsx
+++ src/Components/roomClicked/Body.jsx
@@ -1,9 +1,41 @@
+import { useState } from "react";
 import Carusel_Comp from "../Carausel_Comp";
 import WcIcon from "@mui/icons-material/Wc";
 import StarRoundedIcon from "@mui/icons-material/StarRounded";
 import LibraryBooksRoundedIcon from "@mui/icons-material/LibraryBooksRounded";
+import AddBoxRoundedIcon from "@mui/icons-material/AddBoxRounded";
+import IndeterminateCheckBoxRoundedIcon from "@mui/icons-material/IndeterminateCheckBoxRounded";
+import { TextField, InputAdornment } from "@mui/material";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
 
 function Body() {
+  const [selectedTab, setSelectedTab] = useState("Features");
+  const [isView, setView] = useState([false, false, true, true, true]);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [dates, setDates] = useState({
+    checkIn: "",
+    checkOut: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setDates((prevDates) => ({
+      ...prevDates,
+      [name]: value,
+    }));
+  };
+
+  const togglePolicies = (index) => {
+    setView((prev) => prev.map((view, i) => (i === index ? !view : view)));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email, message);
+  };
+
   return (
     <div>
       <div className="w-full max-w-[100%] sm:max-w-[700px] md:max-w-[1000px] lg:max-w-[1100px] xl:max-w-[1200px] mx-auto">
@@ -42,10 +74,16 @@ function Body() {
           </div>
         </div>
 
+        {/* start of content */}
         <div className="max-w-[1100px]">
-          <div className="w-2/3 h-full shadow-md mt-10 p-4">
+          <div className="w-2/3 max-h-[500px] shadow-md mt-10 p-4">
             <div className="flex flex-row justify-center mx-24 border">
-              <div className="w-full flex justify-start px-4 py-3 bg-golden text-tealBlack text-2xl font-medium">
+              <div
+                className={`w-full flex justify-start px-4 py-3 cursor-pointer ${
+                  selectedTab === "Features" ? "bg-golden" : ""
+                } text-tealBlack text-2xl font-medium`}
+                onClick={() => setSelectedTab("Features")}
+              >
                 <div className="flex flex-row gap-2">
                   Features
                   <StarRoundedIcon
@@ -53,7 +91,12 @@ function Body() {
                   />
                 </div>
               </div>
-              <div className="w-full flex justify-start px-4 py-3 text-tealBlack text-2xl font-medium">
+              <div
+                className={`w-full flex justify-start px-4 py-3 cursor-pointer ${
+                  selectedTab === "Policies" ? "bg-golden" : ""
+                } text-tealBlack text-2xl font-medium`}
+                onClick={() => setSelectedTab("Policies")}
+              >
                 <div className="flex flex-row gap-2">
                   Policies
                   <div className="rotate-45">
@@ -65,78 +108,305 @@ function Body() {
               </div>
             </div>
 
-            <div className="flex m-10">
-              <div className="flex flex-row">
-                <div className="flex flex-col">
-                  <div className="flex flex-row gap-2 mt-2">
-                    <StarRoundedIcon
-                      sx={{ fontSize: "30px", marginTop: "-2px" }}
-                    />{" "}
-                    <span className="text-xl font-medium">
-                      24 hour room service
-                    </span>
+            <div className="flex m-10 h-full">
+              {selectedTab === "Features" ? (
+                <div className="flex flex-row">
+                  <div className="flex flex-col">
+                    <div className="flex flex-row gap-2 mt-2">
+                      <StarRoundedIcon
+                        sx={{ fontSize: "30px", marginTop: "-2px" }}
+                      />{" "}
+                      <span className="text-xl font-medium">
+                        24 hour room service
+                      </span>
+                    </div>
+                    <div className="flex flex-row gap-2 mt-2">
+                      <StarRoundedIcon
+                        sx={{ fontSize: "30px", marginTop: "-2px" }}
+                      />{" "}
+                      <span className="text-xl font-medium">
+                        24 hour room service
+                      </span>
+                    </div>
+                    <div className="flex flex-row gap-2 mt-2">
+                      <StarRoundedIcon
+                        sx={{ fontSize: "30px", marginTop: "-2px" }}
+                      />{" "}
+                      <span className="text-xl font-medium">
+                        24 hour room service
+                      </span>
+                    </div>
+                    <div className="flex flex-row gap-2 mt-2">
+                      <StarRoundedIcon
+                        sx={{ fontSize: "30px", marginTop: "-2px" }}
+                      />{" "}
+                      <span className="text-xl font-medium">
+                        24 hour room service
+                      </span>
+                    </div>
+                    <div className="flex flex-row gap-2 mt-2">
+                      <StarRoundedIcon
+                        sx={{ fontSize: "30px", marginTop: "-2px" }}
+                      />{" "}
+                      <span className="text-xl font-medium">
+                        24 hour room service
+                      </span>
+                    </div>
                   </div>
-                  <div className="flex flex-row gap-2 mt-2">
-                    <StarRoundedIcon
-                      sx={{ fontSize: "30px", marginTop: "-2px" }}
-                    />{" "}
-                    <span className="text-xl font-medium">
-                      24 hour room service
-                    </span>
-                  </div>
-                  <div className="flex flex-row gap-2 mt-2">
-                    <StarRoundedIcon
-                      sx={{ fontSize: "30px", marginTop: "-2px" }}
-                    />{" "}
-                    <span className="text-xl font-medium">
-                      24 hour room service
-                    </span>
-                  </div>
-                  <div className="flex flex-row gap-2 mt-2">
-                    <StarRoundedIcon
-                      sx={{ fontSize: "30px", marginTop: "-2px" }}
-                    />{" "}
-                    <span className="text-xl font-medium">
-                      24 hour room service
-                    </span>
-                  </div>
-                  <div className="flex flex-row gap-2 mt-2">
-                    <StarRoundedIcon
-                      sx={{ fontSize: "30px", marginTop: "-2px" }}
-                    />{" "}
-                    <span className="text-xl font-medium">
-                      24 hour room service
-                    </span>
-                  </div>
-                </div>
 
-                <div className="ml-20">
-                  <div className="flex flex-row gap-2 mt-2">
-                    <StarRoundedIcon
-                      sx={{ fontSize: "30px", marginTop: "-2px" }}
-                    />{" "}
-                    <span className="text-xl font-medium">
-                      24 hour room service
-                    </span>
-                  </div>
-                  <div className="flex flex-row gap-2 mt-2">
-                    <StarRoundedIcon
-                      sx={{ fontSize: "30px", marginTop: "-2px" }}
-                    />{" "}
-                    <span className="text-xl font-medium">
-                      24 hour room service
-                    </span>
-                  </div>
-                  <div className="flex flex-row gap-2 mt-2">
-                    <StarRoundedIcon
-                      sx={{ fontSize: "30px", marginTop: "-2px" }}
-                    />{" "}
-                    <span className="text-xl font-medium">
-                      24 hour room service
-                    </span>
+                  <div className="ml-20">
+                    <div className="flex flex-row gap-2 mt-2">
+                      <StarRoundedIcon
+                        sx={{ fontSize: "30px", marginTop: "-2px" }}
+                      />{" "}
+                      <span className="text-xl font-medium">
+                        24 hour room service
+                      </span>
+                    </div>
+                    <div className="flex flex-row gap-2 mt-2">
+                      <StarRoundedIcon
+                        sx={{ fontSize: "30px", marginTop: "-2px" }}
+                      />{" "}
+                      <span className="text-xl font-medium">
+                        24 hour room service
+                      </span>
+                    </div>
+                    <div className="flex flex-row gap-2 mt-2">
+                      <StarRoundedIcon
+                        sx={{ fontSize: "30px", marginTop: "-2px" }}
+                      />{" "}
+                      <span className="text-xl font-medium">
+                        24 hour room service
+                      </span>
+                    </div>
                   </div>
                 </div>
-              </div>
+              ) : (
+                <div className="flex flex-col">
+                  <div className="flex flex-col gap-2 mt-2">
+                    <div className="flex items-center gap-2">
+                      <div
+                        className="cursor-pointer"
+                        onClick={() => togglePolicies(0)}
+                      >
+                        {isView[0] ? (
+                          <AddBoxRoundedIcon
+                            sx={{ fontSize: "30px", marginTop: "-2px" }}
+                          />
+                        ) : (
+                          <IndeterminateCheckBoxRoundedIcon
+                            sx={{ fontSize: "30px", marginTop: "-2px" }}
+                          />
+                        )}
+                      </div>
+                      <span className="text-xl font-medium text-golden">
+                        Check-in
+                      </span>
+                    </div>
+
+                    <div className={`${isView[0] ? "hidden" : "block"} pl-8`}>
+                      <span className="text-lg font-medium">From 14:00</span>
+                    </div>
+                  </div>
+
+                  <div className="flex flex-col gap-2 mt-2">
+                    <div className="flex items-center gap-2">
+                      <div
+                        className="cursor-pointer"
+                        onClick={() => togglePolicies(1)}
+                      >
+                        {isView[1] ? (
+                          <AddBoxRoundedIcon
+                            sx={{ fontSize: "30px", marginTop: "-2px" }}
+                          />
+                        ) : (
+                          <IndeterminateCheckBoxRoundedIcon
+                            sx={{ fontSize: "30px", marginTop: "-2px" }}
+                          />
+                        )}
+                      </div>
+                      <span className="text-xl font-medium text-golden">
+                        Check-out
+                      </span>
+                    </div>
+
+                    <div className={`${isView[1] ? "hidden" : "block"} pl-8`}>
+                      <span className="text-lg font-medium">Until 10 AM</span>
+                    </div>
+                  </div>
+
+                  <div className="flex flex-col gap-2 mt-2">
+                    <div className="flex items-center gap-2">
+                      <div
+                        className="cursor-pointer"
+                        onClick={() => togglePolicies(2)}
+                      >
+                        {isView[2] ? (
+                          <AddBoxRoundedIcon
+                            sx={{ fontSize: "30px", marginTop: "-2px" }}
+                          />
+                        ) : (
+                          <IndeterminateCheckBoxRoundedIcon
+                            sx={{ fontSize: "30px", marginTop: "-2px" }}
+                          />
+                        )}
+                      </div>
+                      <span className="text-xl font-medium">
+                        Cancelellation/Prepayment
+                      </span>
+                    </div>
+
+                    <div className={`${isView[2] ? "hidden" : "block"} pl-8`}>
+                      <span className="text-lg font-medium">
+                        Cacellation and prepayment vary accordingly read more
+                        about it
+                      </span>
+                    </div>
+                  </div>
+                  <div className="flex flex-col gap-2 mt-2">
+                    <div className="flex items-center gap-2">
+                      <div
+                        className="cursor-pointer"
+                        onClick={() => togglePolicies(3)}
+                      >
+                        {isView[3] ? (
+                          <AddBoxRoundedIcon
+                            sx={{ fontSize: "30px", marginTop: "-2px" }}
+                          />
+                        ) : (
+                          <IndeterminateCheckBoxRoundedIcon
+                            sx={{ fontSize: "30px", marginTop: "-2px" }}
+                          />
+                        )}
+                      </div>
+                      <span className="text-xl font-medium">Pets</span>
+                    </div>
+
+                    <div className={`${isView[3] ? "hidden" : "block"} pl-8`}>
+                      <span className="text-lg font-medium">
+                        Pets aren't allowed
+                      </span>
+                    </div>
+                  </div>
+                  <div className="flex flex-col gap-2 mt-2">
+                    <div className="flex items-center gap-2">
+                      <div
+                        className="cursor-pointer"
+                        onClick={() => togglePolicies(4)}
+                      >
+                        {isView[4] ? (
+                          <AddBoxRoundedIcon
+                            sx={{ fontSize: "30px", marginTop: "-2px" }}
+                          />
+                        ) : (
+                          <IndeterminateCheckBoxRoundedIcon
+                            sx={{ fontSize: "30px", marginTop: "-2px" }}
+                          />
+                        )}
+                      </div>
+                      <span className="text-xl font-medium">Groups</span>
+                    </div>
+
+                    <div className={`${isView[4] ? "hidden" : "block"} pl-8`}>
+                      <span className="text-lg font-medium">
+                        When booking more that 5 rooms, different policies may
+                        apply
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+        {/* end of content */}
+
+        {/* form */}
+        <div className="border mt-10">
+          <div className="flex justify-center">
+            <div className="w-full max-w-[800px] px-4 sm:px-6 lg:px-8">
+              <form onSubmit={handleSubmit} className="flex flex-col p-4 gap-3">
+                <div className="w-full max-w-[325px]">
+                  <label htmlFor="name" className="flex flex-col font-medium">
+                    Full name
+                    <input
+                      id="name"
+                      className="shadow-sm outline-none py-2 px-3 border-2 border-gray-200 focus:border-golden font-normal rounded-sm placeholder-gray-300 w-full"
+                      type="text"
+                      placeholder="Full name"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                    />
+                  </label>
+                </div>
+                <div className="flex flex-row justify-between space-x-[50px]">
+                  <div className="w-full max-w-[350px]">
+                    <label
+                      className="flex flex-col font-medium"
+                      htmlFor="checkin-date"
+                    >
+                      Select check-in date
+                      <TextField
+                        focused={false}
+                        id="check-in"
+                        name="checkIn"
+                        type="date"
+                        value={dates.checkIn}
+                        onChange={handleChange}
+                        variant="outlined"
+                        InputProps={{
+                          startAdornment: (
+                            <InputAdornment position="start">
+                              <CalendarTodayIcon />
+                            </InputAdornment>
+                          ),
+                        }}
+                        fullWidth
+                      />
+                    </label>
+                  </div>
+                  <div className="w-full max-w-[350px]">
+                    <label
+                      className="flex flex-col font-medium"
+                      htmlFor="checkout-date"
+                    >
+                      Select check-out date
+                      <TextField
+                        id="check-out"
+                        name="checkOut"
+                        type="date"
+                        value={dates.checkOut}
+                        onChange={handleChange}
+                        variant="outlined"
+                        InputProps={{
+                          startAdornment: (
+                            <InputAdornment position="start">
+                              <CalendarTodayIcon />
+                            </InputAdornment>
+                          ),
+                        }}
+                        fullWidth
+                      />
+                    </label>
+                  </div>
+                </div>
+                <label className="flex flex-col font-medium" htmlFor="message">
+                  Message
+                  <textarea
+                    id="message"
+                    className="h-32 outline-none py-2 px-3 rounded-sm shadow-sm border-2 border-gray-200 focus:border-golden resize-none overflow-auto font-normal placeholder-gray-300 w-full"
+                    placeholder="Write your message here..."
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                  />
+                </label>
+                <button
+                  type="submit"
+                  className="bg-[#deae54] text-white text-lg px-3 py-2 rounded-sm hover:bg-[#e3b359] w-full"
+                >
+                  Submit
+                </button>
+              </form>
             </div>
           </div>
         </div>
