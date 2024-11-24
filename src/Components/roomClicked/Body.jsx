e437db4a0ab046a65f06290b698e5a49e43caae3
dawitYenew.12 <dawityenew.12@gmail.com>
2024-11-24 22:10:51 +0300

    room clicked with responsiveness done

--- src/Components/roomClicked/Body.jsx
+++ src/Components/roomClicked/Body.jsx
@@ -7,6 +7,7 @@ import AddBoxRoundedIcon from "@mui/icons-material/AddBoxRounded";
 import IndeterminateCheckBoxRoundedIcon from "@mui/icons-material/IndeterminateCheckBoxRounded";
 import { TextField, InputAdornment } from "@mui/material";
 import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
+import BookingForm from "./BookingForm";
 
 function Body() {
   const [selectedTab, setSelectedTab] = useState("Features");
@@ -29,11 +30,6 @@ function Body() {
 
   const togglePolicies = (index) => {
     setView((prev) => prev.map((view, i) => (i === index ? !view : view)));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(name, email, message);
   };
 
   return (
@@ -75,33 +71,40 @@ function Body() {
         </div>
 
         {/* start of content */}
-        <div className="max-w-[1100px]">
-          <div className="w-2/3 max-h-[500px] shadow-md mt-10 p-4">
-            <div className="flex flex-row justify-center mx-24 border">
+        <div className="max-w-[1100px] border px-4 md:p">
+          <div className="w-full md:w-2/3 max-h-[500px] shadow-md ml md:ml-2 mt-10 border p-4">
+            <div className="flex flex-row justify-center mx-4 sm:mx-2 md:mx-24 border">
               <div
-                className={`w-full flex justify-start px-4 py-3 cursor-pointer ${
+                className={`w-full flex justify-start px-2 sm:px-4 py-3 cursor-pointer ${
                   selectedTab === "Features" ? "bg-golden" : ""
-                } text-tealBlack text-2xl font-medium`}
+                } text-tealBlack text-lg sm:text-xl md:text-2xl font-medium`}
                 onClick={() => setSelectedTab("Features")}
               >
-                <div className="flex flex-row gap-2">
+                <div className="flex flex-row gap-1 sm:gap-2 items-center">
                   Features
                   <StarRoundedIcon
-                    sx={{ fontSize: "28px", marginTop: "4px" }}
+                    sx={{
+                      fontSize: window.innerWidth < 768 ? "20px" : "24px",
+                      marginTop: "4px",
+                    }}
                   />
                 </div>
               </div>
+
               <div
-                className={`w-full flex justify-start px-4 py-3 cursor-pointer ${
+                className={`w-full flex justify-start px-2 sm:px-4 py-3 cursor-pointer ${
                   selectedTab === "Policies" ? "bg-golden" : ""
-                } text-tealBlack text-2xl font-medium`}
+                } text-tealBlack text-lg sm:text-xl md:text-2xl font-medium`}
                 onClick={() => setSelectedTab("Policies")}
               >
-                <div className="flex flex-row gap-2">
+                <div className="flex flex-row gap-1 sm:gap-2 items-center">
                   Policies
                   <div className="rotate-45">
                     <LibraryBooksRoundedIcon
-                      sx={{ fontSize: "28px", marginTop: "-2px" }}
+                      sx={{
+                        fontSize: window.innerWidth < 768 ? "20px" : "24px",
+                        marginTop: "-2px",
+                      }}
                     />
                   </div>
                 </div>
@@ -110,75 +113,29 @@ function Body() {
 
             <div className="flex m-10 h-full">
               {selectedTab === "Features" ? (
-                <div className="flex flex-row">
-                  <div className="flex flex-col">
-                    <div className="flex flex-row gap-2 mt-2">
-                      <StarRoundedIcon
-                        sx={{ fontSize: "30px", marginTop: "-2px" }}
-                      />{" "}
-                      <span className="text-xl font-medium">
-                        24 hour room service
-                      </span>
-                    </div>
-                    <div className="flex flex-row gap-2 mt-2">
-                      <StarRoundedIcon
-                        sx={{ fontSize: "30px", marginTop: "-2px" }}
-                      />{" "}
-                      <span className="text-xl font-medium">
-                        24 hour room service
-                      </span>
-                    </div>
-                    <div className="flex flex-row gap-2 mt-2">
-                      <StarRoundedIcon
-                        sx={{ fontSize: "30px", marginTop: "-2px" }}
-                      />{" "}
-                      <span className="text-xl font-medium">
-                        24 hour room service
-                      </span>
-                    </div>
-                    <div className="flex flex-row gap-2 mt-2">
-                      <StarRoundedIcon
-                        sx={{ fontSize: "30px", marginTop: "-2px" }}
-                      />{" "}
-                      <span className="text-xl font-medium">
-                        24 hour room service
-                      </span>
-                    </div>
-                    <div className="flex flex-row gap-2 mt-2">
-                      <StarRoundedIcon
-                        sx={{ fontSize: "30px", marginTop: "-2px" }}
-                      />{" "}
-                      <span className="text-xl font-medium">
-                        24 hour room service
-                      </span>
-                    </div>
+                <div className="flex flex-row flex-wrap md:flex-nowrap justify-center gap-x-8 gap-y-2">
+                  {/* Column 1 */}
+                  <div className="flex flex-col gap-2">
+                    {Array(5)
+                      .fill("24 hour room service")
+                      .map((item, index) => (
+                        <div className="flex items-center gap-2" key={index}>
+                          <StarRoundedIcon sx={{ fontSize: window.innerWidth < 768 ? "20px" : "24px" }} />
+                          <span className="text-xl font-medium">{item}</span>
+                        </div>
+                      ))}
                   </div>
 
-                  <div className="ml-20">
-                    <div className="flex flex-row gap-2 mt-2">
-                      <StarRoundedIcon
-                        sx={{ fontSize: "30px", marginTop: "-2px" }}
-                      />{" "}
-                      <span className="text-xl font-medium">
-                        24 hour room service
-                      </span>
-                    </div>
-                    <div className="flex flex-row gap-2 mt-2">
-                      <StarRoundedIcon
-                        sx={{ fontSize: "30px", marginTop: "-2px" }}
-                      />{" "}
-                      <span className="text-xl font-medium">
-                        24 hour room service
-                      </span>
-                    </div>
-                    <div className="flex flex-row gap-2 mt-2">
-                      <StarRoundedIcon
-                        sx={{ fontSize: "30px", marginTop: "-2px" }}
-                      />{" "}
-                      <span className="text-xl font-medium">
-                        24 hour room service
-                      </span>
-                    </div>
+                  {/* Column 2 */}
+                  <div className="flex flex-col gap-2">
+                    {Array(3)
+                      .fill("24 hour room service")
+                      .map((item, index) => (
+                        <div className="flex items-center gap-2" key={index}>
+                          <StarRoundedIcon sx={{ fontSize: window.innerWidth < 768 ? "20px" : "24px" }} />
+                          <span className="text-xl font-medium">{item}</span>
+                        </div>
+                      ))}
                   </div>
                 </div>
               ) : (
@@ -322,93 +279,8 @@ function Body() {
         {/* end of content */}
 
         {/* form */}
-        <div className="border mt-10">
-          <div className="flex justify-center">
-            <div className="w-full max-w-[800px] px-4 sm:px-6 lg:px-8">
-              <form onSubmit={handleSubmit} className="flex flex-col p-4 gap-3">
-                <div className="w-full max-w-[325px]">
-                  <label htmlFor="name" className="flex flex-col font-medium">
-                    Full name
-                    <input
-                      id="name"
-                      className="shadow-sm outline-none py-2 px-3 border-2 border-gray-200 focus:border-golden font-normal rounded-sm placeholder-gray-300 w-full"
-                      type="text"
-                      placeholder="Full name"
-                      value={name}
-                      onChange={(e) => setName(e.target.value)}
-                    />
-                  </label>
-                </div>
-                <div className="flex flex-row justify-between space-x-[50px]">
-                  <div className="w-full max-w-[350px]">
-                    <label
-                      className="flex flex-col font-medium"
-                      htmlFor="checkin-date"
-                    >
-                      Select check-in date
-                      <TextField
-                        focused={false}
-                        id="check-in"
-                        name="checkIn"
-                        type="date"
-                        value={dates.checkIn}
-                        onChange={handleChange}
-                        variant="outlined"
-                        InputProps={{
-                          startAdornment: (
-                            <InputAdornment position="start">
-                              <CalendarTodayIcon />
-                            </InputAdornment>
-                          ),
-                        }}
-                        fullWidth
-                      />
-                    </label>
-                  </div>
-                  <div className="w-full max-w-[350px]">
-                    <label
-                      className="flex flex-col font-medium"
-                      htmlFor="checkout-date"
-                    >
-                      Select check-out date
-                      <TextField
-                        id="check-out"
-                        name="checkOut"
-                        type="date"
-                        value={dates.checkOut}
-                        onChange={handleChange}
-                        variant="outlined"
-                        InputProps={{
-                          startAdornment: (
-                            <InputAdornment position="start">
-                              <CalendarTodayIcon />
-                            </InputAdornment>
-                          ),
-                        }}
-                        fullWidth
-                      />
-                    </label>
-                  </div>
-                </div>
-                <label className="flex flex-col font-medium" htmlFor="message">
-                  Message
-                  <textarea
-                    id="message"
-                    className="h-32 outline-none py-2 px-3 rounded-sm shadow-sm border-2 border-gray-200 focus:border-golden resize-none overflow-auto font-normal placeholder-gray-300 w-full"
-                    placeholder="Write your message here..."
-                    value={message}
-                    onChange={(e) => setMessage(e.target.value)}
-                  />
-                </label>
-                <button
-                  type="submit"
-                  className="bg-[#deae54] text-white text-lg px-3 py-2 rounded-sm hover:bg-[#e3b359] w-full"
-                >
-                  Submit
-                </button>
-              </form>
-            </div>
-          </div>
+        <div className="mt-10">
+          <BookingForm />
         </div>
       </div>
     </div>
