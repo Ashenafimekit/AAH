4168c13a56dd4ad3794eef9fa47ad9c7f074e6bf
Ashenafi Mekit <ashumekit502@gmail.com>
2024-12-09 21:18:13 +0300

    some change

--- backend/loader/express.js
+++ backend/loader/express.js
@@ -17,11 +17,15 @@ const __dirname = path.dirname(__filename);
 
 export default async (app) => {
   app.use(express.json());
+  const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true,
+  };
   if (config.env === 'production') {
     app.use(cors({ origin: url }));
     app.options('*', cors({ origin: url }));
   } else {
-    app.use(cors());
+    app.use(cors(corsOptions));
   }
 
   app.use('/uploads', express.static(path.join(__dirname, '../../uploads')));
